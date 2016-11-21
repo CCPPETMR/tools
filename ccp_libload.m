a9e43239d93e329ec5da30a7721e63f7ab0184ba
David Atkinson <D.Atkinson@ucl.ac.uk>
2016-11-21 21:53:09 +0000

    Add files via upload

---
+++ ccp_libload.m
@@ -0,0 +1,18 @@
+function ccp_libload
+% CCP_LIBLOAD Load mutilities and mgadgetron libraries for CCP code
+%
+% Usage:
+%  ccp_libload
+%
+% Extracted by David Atkinson from code by Evgueni Ovtchinnikov
+% 
+
+if ~libisloaded('mutilities')
+    fprintf('loading mutilities library...\n')
+    [notfound, warnings] = loadlibrary('mutilities');
+end
+
+if ~libisloaded('mgadgetron')
+    fprintf('loading mgadgetron library...\n')
+    [notfound, warnings] = loadlibrary('mgadgetron');
+end
